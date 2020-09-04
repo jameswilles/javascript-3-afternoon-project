b8d7ae51d6a9708b84273a85db1c91e9908e4e4d
jameswilles <james.m.willes@gmail.com>
2020-09-04 14:35:25 -0600

    destructuring problems complete

--- destructuring.js
+++ destructuring.js
@@ -22,7 +22,7 @@ var carDetails = {
 */
 
 //Code Here
-
+let {color, make, model, year} = carDetails
 
 
 ////////// PROBLEM 2 //////////
@@ -35,7 +35,7 @@ var carDetails = {
 
 function greeting( obj ) {
   //Code Here
-  
+  let {firstName, lastName, title} = obj;
   // Do not edit the code below.
   return 'Hello, ' + title + ' ' + firstName + ' ' + lastName + '!';
   // Do not edit the code above.
@@ -54,6 +54,10 @@ function greeting( obj ) {
 */
 
 //Code Here
+function totalPopulation (obj) {
+  let {utah, california, texas, arizona} = obj;
+  return utah + california + texas + arizona;
+}
 
 
 
@@ -68,6 +72,12 @@ function greeting( obj ) {
 */
 
 //Code Here
+function ingredients(obj) {
+  let {carb, fat, protein} = obj;
+  let arr = []
+  arr.push(carb, fat, protein)
+  return arr
+}
 
 
 
@@ -86,6 +96,15 @@ function greeting( obj ) {
 */
 
 //Code Here
+function largeNumbers( {first, second, third} ){
+  if(first < second && first < third){
+    return first
+  } else if(second < first && second < third) {
+    return second
+  } else {
+    return third
+  }
+}
 
 
 
@@ -98,5 +117,14 @@ function greeting( obj ) {
 */
 
 //Code Here
+function numberGroups( {a, b, c} ) {
+  if(a.length > b.length && a.length > c.length) {
+    return a
+  } else if (b.length > a.length && b.length > c.length) {
+    return b
+  } else {
+    return c
+  }
+}
 
 
