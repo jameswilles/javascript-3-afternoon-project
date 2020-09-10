1fb31e9eb84650dfee679841be0890721578267d
jameswilles <james.m.willes@gmail.com>
2020-09-10 14:46:38 -0600

    higher order actually complete

--- higher-order.js
+++ higher-order.js
@@ -126,5 +126,11 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method to create to get the sum of bobsTotal.
 */
 let bobsPurchaces = purchases.filter((el) => el.owner === 'Bob')
-let bobsTotal = bobsPurchaces.reduce((acc, el) => acc + el.price)
+let bobsTotal = bobsPurchaces.reduce((acc, el) => {return acc + el.price}, 0)
+
+// let bobsTotal = purchaces.reduce((accumulator, element) => {
+//   if (element.owner === 'Bob') {
+//     accumulator + element.price
+//   }
+// })
 
